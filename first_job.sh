72730e2350c7556652796cd54c502c4537329c9a
KenSingson <kennicolassingson@gmail.com>
2022-03-25 09:37:57 +0800

    Update first_job.sh

--- first_job.sh
+++ first_job.sh
@@ -1,1 +1,1 @@
-echo "This is my from a file triggered by Jenkins with GitHub Integration. With permission set to 774."
+echo "Second Testing. This is my from a file triggered by Jenkins with GitHub Integration. With permission set to 774."
